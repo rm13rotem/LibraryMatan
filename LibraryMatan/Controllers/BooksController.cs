23b87e496d331f83e4fe7325438cbeea6a43800e
Rotem Meron <rm13rotem@gmail.com>
2021-04-01 20:58:07 +0300

    fixed book search ללא סינון קודם

--- LibraryMatan/Controllers/BooksController.cs
+++ LibraryMatan/Controllers/BooksController.cs
@@ -35,10 +35,10 @@ namespace LibraryMatan.Controllers
                 newBooks = newBooks.Where(x => x.Author.Contains(filter.Author)).ToList();
             else if (filter.GenreId > 0)
                 newBooks = newBooks.Where(x => x.GenreId == filter.GenreId).ToList();
-            else newBooks = newBooks.Where(x => x.CreatedDateTime > DateTime.Now.AddMonths(-1)).ToList();
+            else newBooks = newBooks.OrderBy(x=>x.RequestedTimes).ThenByDescending(x=>x.CreatedDateTime).Take(20).ToList();
             filter.Result = newBooks;
             var genres = genreRepository.GetAll().ToList();
-            genres.Add(new Genre() { Id = 0, GenreName = "ללא סינון" });
+            genres.Insert(0,new Genre() { Id = 0, GenreName = "ללא סינון" });
             ViewBag.GenreId = genres;
             return View(filter);
         }
